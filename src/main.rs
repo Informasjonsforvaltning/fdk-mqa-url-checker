1590536325c8da9c69ef6a8bc6ba7d6a9f903764
Amund Tenstad <github@amund.io>
2022-08-03 10:10:35 +0200

    chore: refactor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::time::Duration;
-
 use futures::{stream::FuturesUnordered, StreamExt};
-use log::{error, info};
-use schema_registry_converter::async_impl::schema_registry::SrSettings;
 
 use crate::{
-    kafka::{BROKERS, INPUT_TOPIC, OUTPUT_TOPIC, SCHEMA_REGISTRY},
+    kafka::{
+        create_sr_settings, run_async_processor, BROKERS, INPUT_TOPIC, OUTPUT_TOPIC,
+        SCHEMA_REGISTRY,
+    },
     schemas::setup_schemas,
 };
 
@@ -33,38 +32,27 @@ async fn main() {
         "starting service"
     );
 
-    let mut schema_registry_urls = SCHEMA_REGISTRY.split(",");
-    let mut sr_settings_builder =
-        SrSettings::new_builder(schema_registry_urls.next().unwrap().to_string());
-    schema_registry_urls.for_each(|url| {
-        sr_settings_builder.add_url(url.to_string());
-    });
-
-    let sr_settings = sr_settings_builder
-        .set_timeout(Duration::from_secs(5))
-        .build()
-        .unwrap_or_else(|_| {
-            error!("unable to create SrSettings");
-            std::process::exit(1);
-        });
-
-    let id = setup_schemas(&sr_settings).await.unwrap_or_else(|_| {
-        error!("unable to register schemas");
+    let sr_settings = create_sr_settings().unwrap_or_else(|e| {
+        tracing::error!(error = e.to_string(), "sr settings creation error");
         std::process::exit(1);
     });
-    info!("Schema succesfully registered with id={}", id);
+
+    setup_schemas(&sr_settings).await.unwrap_or_else(|e| {
+        tracing::error!(error = e.to_string(), "schema registration error");
+        std::process::exit(1);
+    });
 
     (0..4)
-        .map(|i| tokio::spawn(kafka::run_async_processor(i, sr_settings.clone())))
+        .map(|i| tokio::spawn(run_async_processor(i, sr_settings.clone())))
         .collect::<FuturesUnordered<_>>()
         .for_each(|result| async {
             result
-                .unwrap_or_else(|_| {
-                    error!("unable to run worker thread");
+                .unwrap_or_else(|e| {
+                    tracing::error!(error = e.to_string(), "unable to run worker thread");
                     std::process::exit(1);
                 })
-                .unwrap_or_else(|_| {
-                    error!("worker failed");
+                .unwrap_or_else(|e| {
+                    tracing::error!(error = e.to_string(), "worker failed");
                     std::process::exit(1);
                 });
         })
