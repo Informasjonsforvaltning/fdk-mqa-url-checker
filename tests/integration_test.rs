b4cbe7b8eac82c6e948b9555a39cc2d68968988b
Nils Ove Tendenes <nils.ove@tendenes.com>
2024-12-17 08:33:56 +0100

    chore: update oxigraph

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -11,6 +11,10 @@ use kafka_utils::{consume_all_messages, receive_message, AvroProducer};
 use oxigraph::store::Store;
 use rdkafka::consumer::StreamConsumer;
 use schema_registry_converter::async_impl::avro::{AvroDecoder, AvroEncoder};
+use sophia_api::term::SimpleTerm;
+use sophia_api::source::TripleSource;
+use sophia_isomorphism::isomorphic_graphs;
+use sophia_turtle::parser::turtle::parse_str;
 use uuid::Uuid;
 
 use crate::kafka_utils::AvroConsumer;
@@ -83,31 +87,12 @@ async fn assert_transformation(input: &str, output: &str) {
     // Consume message produced by url-checker.
     let message = consumer.receive_message::<MqaEvent>().await.unwrap();
 
-    assert_eq!(
-        sorted_lines(replace_blank(&message.graph)),
-        sorted_lines(replace_blank(&output))
-    );
-}
+    let result_graph: Vec<[SimpleTerm; 3]> = parse_str(&message.graph.as_str())
+        .collect_triples()
+        .unwrap();
+    let expected_graph: Vec<[SimpleTerm; 3]> = parse_str(&output)
+        .collect_triples()
+        .unwrap();
 
-pub fn replace_blank(text: &str) -> String {
-    let mut chars = text.chars().collect::<Vec<char>>();
-    for i in (0..(chars.len() - 2)).rev() {
-        if chars[i] == '_' && chars[i + 1] == ':' {
-            while chars[i] != ' ' {
-                chars.remove(i);
-            }
-            chars.insert(i, 'b')
-        }
-    }
-    chars.iter().collect::<String>()
-}
-
-pub fn sorted_lines(text: String) -> Vec<String> {
-    let mut lines: Vec<String> = text
-        .split("\n")
-        .map(|l| l.trim().to_string())
-        .filter(|l| l.len() > 0)
-        .collect();
-    lines.sort();
-    lines
+    assert!(isomorphic_graphs(&expected_graph, &result_graph).unwrap())
 }
